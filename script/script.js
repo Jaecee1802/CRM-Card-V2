b18c901dd1419ae43ebd9d60e65fcf797abffe9d
JC Banzuela <162450651+Jaecee1802@users.noreply.github.com>
2024-12-01 12:20:43 +0800

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -9,9 +9,9 @@ const github = document.getElementById("git");
 cardBackground.style.backgroundColor = "rgba(0,0,0, 0.5)";
 cardBackground.style.padding = "10vh"
 textAlignment.style.textAlign = "center";
-userName.innerHTML = "<h1>CRM</h1>";
-userName.style.fontFamily = "Righteous, sans-serif";
-socialFont.style.fontFamily = "Raleway, sans-serif";
+userName.innerHTML = "<h1>Crm</h1>";
+userName.style.fontFamily = "Silkscreen, sans-serif";
+socialFont.style.fontFamily = "Silkscreen, sans-serif";
 
 instaGram.innerHTML = "Instagram";
 instaGram.style.marginTop = "4px";
@@ -22,11 +22,11 @@ steam.style.marginTop = "4px";
 github.innerHTML = "Github";
 github.style.marginTop = "4px";
 
-instaGram.style.fontFamily = "Raleway, sans-serif";
+instaGram.style.fontFamily = "Silkscreen, sans-serif";
 instaGram.style.fontWeight = "300";
 
-steam.style.fontFamily = "Raleway, sans-serif";
+steam.style.fontFamily = "Silkscreen, sans-serif";
 steam.style.fontWeight = "300";
 
-github.style.fontFamily = "Raleway, sans-serif";
+github.style.fontFamily = "Silkscreen, sans-serif";
 github.style.fontWeight = "300";
